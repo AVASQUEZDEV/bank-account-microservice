57a22d5c813874e642be1b6627346b32fb108e60
Alcibar Celso Vasquez Sanchez <avasqusa@emeal.nttdata.com>
2022-05-06 12:30:28 -0500

    fix: bank accounts

--- src/test/java/com/nttdata/bankaccount/BankaccountApplicationTests.java
+++ src/test/java/com/nttdata/bankaccount/BankaccountApplicationTests.java
@@ -1,19 +1,5 @@
 package com.nttdata.bankaccount;
 
-import com.nttdata.bankaccount.controller.BankAccountRestController;
-import com.nttdata.bankaccount.controller.CardTypeRestController;
-import com.nttdata.bankaccount.dto.response.BankAccountResponse;
-import com.nttdata.bankaccount.dto.response.CardTypeResponse;
-import com.nttdata.bankaccount.service.IBankAccountService;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.reactive.server.WebTestClient;
-
 class BankaccountApplicationTests {
 
 	
